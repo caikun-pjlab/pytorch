0541a2529a13eb7e1cce3698125420e66aea6df4
voznesenskym <voznesenskym@gmail.com>
2024-01-18 19:51:46 -0800

    Replace invoking self.value with inlining to trace into it, avoiding arbitrary code execution
    ghstack-source-id: 5fa249d9126eae1bae07cdf6eef29ca8a74e7d0f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117818
    
    Replace invoking self.value with inlining to trace into it, avoiding arbitrary code execution
    
    Test
    
    Test

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -291,9 +291,22 @@ class UserDefinedClassVariable(UserDefinedVariable):
             and len(kwargs) == 0  # TODO(ybliang): support kwargs
         ):
             unwrapped_args = [x.as_python_constant() for x in args]
-            return GenericContextWrappingVariable(
-                unwrapped_args,
-                cm_obj=self.value(*unwrapped_args),
+            if self.value.__init__ == object.__init__:
+                return GenericContextWrappingVariable(
+                    unwrapped_args,
+                    cm_obj=self.value(*unwrapped_args),
+                )
+
+            try:
+                fn = variables.UserFunctionVariable(
+                    self.value.__init__, source=self.source
+                )
+            except AssertionError:
+                unimplemented("Failed to get __init__ function of %s" % self.value)
+            return tx.inline_user_function_return(
+                fn,
+                (self, *args),
+                {},
             )
         elif is_namedtuple_cls(self.value):
             fields = namedtuple_fields(self.value)
